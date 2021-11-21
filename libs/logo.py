7c57af126a6bf67e2c8cac72dc8e1d64887a35f6
Mr-JK9 <71639297+Mr-JK9@users.noreply.github.com>
2021-11-21 22:19:32 +0530

    Add files via upload

---
+++ libs/logo.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+#!/usr/bin/env python3
+from colorama import Fore, Back, Style
+from random import choice
+
+logo =  """
+
+██╗███╗░░██╗░██████╗████████╗░█████╗░    ██████╗░███████╗██████╗░░█████╗░██████╗░████████╗
+██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗    ██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝
+██║██╔██╗██║╚█████╗░░░░██║░░░███████║    ██████╔╝█████╗░░██████╔╝██║░░██║██████╔╝░░░██║░░░
+██║██║╚████║░╚═══██╗░░░██║░░░██╔══██║    ██╔══██╗██╔══╝░░██╔═══╝░██║░░██║██╔══██╗░░░██║░░░
+██║██║░╚███║██████╔╝░░░██║░░░██║░░██║    ██║░░██║███████╗██║░░░░░╚█████╔╝██║░░██║░░░██║░░░
+╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝    ╚═╝░░╚═╝╚══════╝╚═╝░░░░░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░      
+                                                                   
+                                                                   """BY - JUGAL KAPDE
+
+
+def print_logo():
+    print(Fore.RED + Style.BRIGHT + logo + Style.RESET_ALL + Style.BRIGHT +"\n")
+    print(Style.RESET_ALL + Style.BRIGHT, Style.BRIGHT)                                                  
+    print(Fore.RED + "                                                      Develope By" + Fore.CYAN + " - Mr_Jk9 ")
+    print(Fore.RED + "                                                      Website" + Fore.CYAN + " - www.Jugalkapde.tk")
+    print(Fore.RED + "                                                      Telegram"+ Fore.CYAN + " - t.me/MrJK9 ")
+    print(Fore.GREEN + "      Mr_JK9 Project: https://Jugalkapde.tk"+ Style.RESET_ALL + Style.BRIGHT)
